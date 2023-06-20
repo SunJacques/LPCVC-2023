049919039d5391d7ba05398e3fc6aa56127cd8ef
Noe Vernier <nvernier-22@gpu5.enst.fr>
2023-06-20 14:48:18 +0200

    Merging dataset files

--- src/dataset.py
+++ src/dataset.py
@@ -11,9 +11,12 @@ import os
 import numpy as np
 
 class LPCVCDataset(Dataset):
-    def __init__(self, img_path, segm_path, train=True, patch=False):
+    def __init__(self, img_path, segm_path, transform,  n_class=14, train=True, patch=False):
         self.img_path = img_path
         self.segm_path = segm_path
+
+        self.transform = transform
+        self.n_class = n_class
 
         self.patches = patch
     
@@ -25,10 +28,15 @@ class LPCVCDataset(Dataset):
         img = cv2.imread(self.img_path + 'train_' + str(idx).zfill(4) + '.png')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         mask = cv2.imread(self.segm_path  + 'train_' +str(idx).zfill(4) + '.png')
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
         
         t = T.Compose([T.ToTensor(), T.Normalize(0, 1)])
         img = t(img)
-        mask = self.onehot(mask,14)
+        mask = self.onehot(mask, self.n_class)
             
         return img, mask
     
