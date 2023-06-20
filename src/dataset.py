6c4a7ed93a6c7f6699fd113cd4f5b8335b6b59a3
Noe Vernier <nvernier-22@gpu5.enst.fr>
2023-06-20 13:38:48 +0200

    Defined Dataset Class

--- src/dataset.py
+++ src/dataset.py
@@ -6,49 +6,34 @@ import matplotlib.pyplot as plt
 import cv2
 from PIL import Image
 from torchvision import transforms as T
-
+import glob
+import os
+import numpy as np
 
 class LPCVCDataset(Dataset):
-    def __init__(self, img_path, segm_path, X, mean, std, transform=None, patch=False):
+    def __init__(self, img_path, segm_path, train=True, patch=False):
         self.img_path = img_path
         self.segm_path = segm_path
-        self.transform = transform
+
         self.patches = patch
-        self.mean = mean
-        self.std = std
-        
+    
     def __len__(self):
-        return len(self.X)
+        files = glob.glob(os.path.join(self.img_path, "*.png"))
+        return len(files)
     
     def __getitem__(self, idx):
-        img = cv2.imread(self.img_path + self.X[idx] + '.jpg')
+        img = cv2.imread(self.img_path + 'train_' + str(idx).zfill(4) + '.png')
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        mask = cv2.imread(self.mask_path + self.X[idx] + '.png', cv2.IMREAD_GRAYSCALE)
+        mask = cv2.imread(self.segm_path  + 'train_' +str(idx).zfill(4) + '.png')
         
-        if self.transform is not None:
-            aug = self.transform(image=img, mask=mask)
-            img = Image.fromarray(aug['image'])
-            mask = aug['mask']
-        
-        if self.transform is None:
-            img = Image.fromarray(img)
-        
-        t = T.Compose([T.ToTensor(), T.Normalize(self.mean, self.std)])
+        t = T.Compose([T.ToTensor(), T.Normalize(0, 1)])
         img = t(img)
-        mask = torch.from_numpy(mask).long()
-        
-        if self.patches:
-            img, mask = self.tiles(img, mask)
+        mask = self.onehot(mask,14)
             
         return img, mask
     
-    def tiles(self, img, mask):
-
-        img_patches = img.unfold(1, 512, 512).unfold(2, 768, 768) 
-        img_patches  = img_patches.contiguous().view(3,-1, 512, 768) 
-        img_patches = img_patches.permute(1,0,2,3)
-        
-        mask_patches = mask.unfold(0, 512, 512).unfold(1, 768, 768)
-        mask_patches = mask_patches.contiguous().view(-1, 512, 768)
-        
-        return img_patches, mask_patches
+    def onehot(self, img, nb):
+        oh = np.zeros((img.shape[0], img.shape[1], nb))
+        for i in range(nb):
+            oh[:,:,i] = (img[:,:,0] == i)
+        return oh
