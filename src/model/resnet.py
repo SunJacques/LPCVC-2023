38478105a3aab2e6e69a2203292d291fe7a55ab4
Ulysse Ristorcelli <uristorc-22@gpu2.enst.fr>
2023-06-21 09:23:47 +0200

    final resnet 18 to 152

--- src/model/resnet.py
+++ src/model/resnet.py
@@ -1,6 +1,117 @@
+import torch
+import torch.nn as nn
+
+from torchvision import models
+
+
+class BottleNeck(nn.Module):
+    # Scale factor of the number of output channels
+    expansion = 4
+
+    def __init__(self, in_channels, out_channels, stride=1, is_first_block=False):
+        """
+        Args: 
+            in_channels: number of input channels
+            out_channels: number of output channels
+            stride: stride using in (a) 3x3 convolution and 
+                    (b) 1x1 convolution used for downsampling for skip connection
+            is_first_block: whether it is the first residual block of the layer
+        """
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.conv3 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels*self.expansion, kernel_size=1, stride=1, padding=0)
+        self.bn3 = nn.BatchNorm2d(out_channels*self.expansion)
+
+        self.relu = nn.ReLU()
+
+        # Skip connection goes through 1x1 convolution with stride=2 for 
+        # the first blocks of conv3_x, conv4_x, and conv5_x layers for matching
+        # spatial dimension of feature maps and number of channels in order to 
+        # perform the add operations.
+        self.downsample = None
+        if is_first_block:
+            self.downsample = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels*self.expansion, kernel_size=1, stride=stride, padding=0), nn.BatchNorm2d(out_channels*self.expansion))
+
+
+    def forward(self, x):
+        """
+        Args:
+            x: input
+        Returns:
+            Residual block output
+        """
+        identity = x.clone()
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.downsample:
+            identity = self.downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+
+        return x
+
+
+class BasicBlock(nn.Module):
+    # Scale factor of the number of output channels
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1, is_first_block=False):
+        """
+        Args: 
+            in_channels: number of input channels
+            out_channels: number of output channels
+            stride: stride using in (a) the first 3x3 convolution and 
+                    (b) 1x1 convolution used for downsampling for skip connection
+            is_first_block: whether it is the first residual block of the layer
+        """
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.relu = nn.ReLU()
+
+        # Skip connection goes through 1x1 convolution with stride=2 for 
+        # the first blocks of conv3_x, conv4_x, and conv5_x layers for matching
+        # spatial dimension of feature maps and number of channels in order to 
+        # perform the add operations.
+        self.downsample = None
+        if is_first_block and stride != 1:
+            self.downsample = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride, padding=0), nn.BatchNorm2d(out_channels))
+
+
+    def forward(self, x):
+        """
+        Args:
+            x: input
+        Returns:
+            Residual block ouput
+        """
+        identity = x.clone()
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.bn2(self.conv2(x))
+
+        if self.downsample:
+            identity = self.downsample(identity)
+        x += identity
+        x = self.relu(x)
+
+        return x
+
+
 class ResNet(nn.Module):
-    def __init__(self, ResBlock, n_classes, n_blocks_list=[3, 4, 6, 3],
-                 out_channels_list=[64, 128, 256, 512], num_channels=3):
+    def __init__(self, ResBlock, n_classes, n_blocks_list=[3, 4, 6, 3], out_channels_list=[64, 128, 256, 512], num_channels=3):
         """
         Args:
             ResBlock: residual block type, BasicBlock for ResNet-18, 34 or 
@@ -15,7 +126,7 @@ class ResNet(nn.Module):
         # First layer
         self.conv1 = nn.Sequential(nn.Conv2d(in_channels=num_channels, out_channels=64, kernel_size=7, stride=2, padding=3), nn.BatchNorm2d(64), nn.ReLU(), nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
 
-        # Create four convolutional layers
+        # Create four convoluiontal layers
         in_channels = 64
         # For the first block of the second layer, do not downsample and use stride=1.
         self.conv2_x = self.CreateLayer(ResBlock, n_blocks_list[0], in_channels, out_channels_list[0], stride=1)
@@ -88,3 +199,61 @@ class ResNet(nn.Module):
                 layer.append(ResBlock(out_channels*ResBlock.expansion, out_channels))
 
         return nn.Sequential(*layer)
+
+
+def GetFeatureMapsFromResnet(net, x):
+    """
+    Args:
+        net: network input from torchvision.
+        x: input image
+    Returns:
+        C2: feature maps after conv2_x
+        C3: feature maps after conv3_x
+        C4: feature maps after conv4_x
+        C5: feature maps after conv5_x
+    """
+    x = net.conv1(x)
+    x = net.bn1(x)
+    x = net.relu(x)
+    x = net.maxpool(x)
+    C2 = net.layer1(x)
+    C3 = net.layer2(C2)
+    C4 = net.layer3(C3)
+    C5 = net.layer4(C4)
+    return C2, C3, C4, C5
+
+
+if __name__ == "__main__":
+    ### Customed version ###
+    # Resnet18
+    #net = ResNet(BasicBlock, 1000, n_blocks_list=[2, 2, 2, 2])
+    # Resnet34
+    #net = ResNet(BasicBlock, 1000)
+    # Resnet50
+    net = ResNet(BottleNeck, 1000)
+    # Resnet101
+    #net = ResNet(BottleNeck, 1000, n_blocks_list=[3, 4, 23, 3])
+    # Resnet152
+    #net = ResNet(BottleNeck, 1000, n_blocks_list=[3, 8, 36, 3])
+    x = torch.randn((1, 3, 512, 800), dtype=torch.float32)
+    C2, C3, C4, C5, _ = net(x)
+
+    ### torchvision version ###
+    #net_tv = models.resnet18(pretrained=False)
+    #net_tv = models.resnet34(pretrained=False)
+    net_tv = models.resnet50(pretrained=False)
+    #net_tv = models.resnet101(pretrained=False)
+    #net_tv = models.resnet152(pretrained=False)
+    C2_tv, C3_tv, C4_tv, C5_tv = GetFeatureMapsFromResnet(net_tv, x)
+
+    print("Verifying the feature map shapes of customed ResNet and ResNet from torchvision")
+    print(f"C2.shape of customed ResNet: {C2.shape}")
+    print(f"C2.shape of torchvision ResNet: {C2_tv.shape}")
+    print(f"C3.shape of customed ResNet: {C3.shape}")
+    print(f"C3.shape of torchvision ResNet: {C3_tv.shape}")
+    print(f"C4.shape of customed ResNet: {C4.shape}")
+    print(f"C4.shape of torchvision ResNet: {C4_tv.shape}")
+    print(f"C5.shape of customed ResNet: {C5.shape}")
+    print(f"C5.shape of torchvision ResNet: {C5_tv.shape}")
+    
+    print("Done!")
