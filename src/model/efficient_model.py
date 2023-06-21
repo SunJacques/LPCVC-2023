cbe5f88aa8d3c651d2d6c4944b6a17487b2fd9d6
Ulysse Ristorcelli <uristorc-22@gpu2.enst.fr>
2023-06-21 14:22:15 +0200

    EfficientNet class implemented

--- src/model/efficient_model.py
+++ src/model/efficient_model.py
@@ -75,10 +75,10 @@ class InvertedResidualBlock(nn.Module):
         survival_prob = 0.8 # for stochastic depth
     ):
         super(InvertedResidualBlock, self).__init__()
-        self.survival_prob = 0;8
+        self.survival_prob = 0.8
         self.use_residual = in_channels == out_channels and stride == 1
         hidden_dim = in_channels * expand_ratio
-        self.expand  in_channels != hidden_dim
+        self.expand = in_channels != hidden_dim
         reduced_dim = int(in_channels / reduction)
 
         if self.expand:
@@ -111,4 +111,50 @@ class InvertedResidualBlock(nn.Module):
             self.conv(x)
 
 class EfficientNet(nn.Module):
-    pass
+    def __init__(self, version, num_classes):
+        super(EfficientNet, self).__init__()
+        width_factor, depth_factor, dropout_rate = self.calculate_factors(version)
+        last_channels = ceil(1280 * width_factor)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.features = self.create_features(width_factor, depth_factor, last_channels)
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(last_channels, num_classes),
+        )
+
+    def calculate_factors(self, version, alpha=1.2, beta= 1.1):
+        phi, res, drop_rate= phi_values[version]
+        depth_factor= alpha ** phi # How larger the channels should be
+        width_factor = beta ** phi
+        return width_factor, depth_factor, drop_rate
+
+    def create_features(self, width_factor, depth_factor, last_channels):
+        channels = int(32 * width_factor)
+        features = [CNNBlock(3, channels, 3, stride=2, padding=1)]
+        in_channels= channels
+
+        for expand_ratio, channels, repeats, stride, kernel_size in base_model:
+            out_channels = 4 * ceil(int(channels*width_factor) / 4) # always need to be divisible by resolution = 4
+            layers_repeats = ceil(repeats * depth_factor)
+
+            for layer in lrange(layers_repeats):
+                features.append(
+                    InvertedResidualBlock(
+                        in_channels,
+                        out_channels,
+                        expand_ratio = expand_ratio,
+                        stride = stride if layer == 0 else 1,
+                        kernel_size = kernel_size,
+                        padding = kernel_size //2,  # if k=1:paad=0; k=3:pad=1; k=5:pad=2
+                    )
+                )
+
+                in_channels = out_channels
+        features.append(
+            CNNBlock(in_channels, last_channels, kernel_size=1, stride=1, padding=0)
+        )
+
+        return nn.Sequential(*features)
+    def forward(self, x):
+        x = self.pool(self.features(x))
+        return self.classifier(x.view(x.shape[0], -1)) # we flatten it before running it through the linear layer
