3c55e03425ebadc1b6b93498c0969c1e246784db
Ulysse Ristorcelli <uristorc-22@gpu2.enst.fr>
2023-06-21 11:31:48 +0200

    InvertedResidualBlock class implemented

--- src/model/efficient_model.py
+++ src/model/efficient_model.py
@@ -30,7 +30,15 @@ phi_values = {
 class CNNBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, groups=1):
         super(CNNBlock, self).__init__()
-        self.cnn = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, groups=groups) 
+        self.cnn = nn.Conv2d(
+            in_channels, 
+            out_channels, 
+            kernel_size, 
+            stride, 
+            padding, 
+            groups=groups,
+            bias = False,
+            ) 
         # If we set groups=1, then it's a normal conv; if we set groups=in_channels, then it's a Depthwise conv
 
         self.bn = nn.BatchNorm2d(out_channels)
@@ -55,7 +63,52 @@ class SqueezeExcitation(nn.Module):
         return x * self.se(x) # how each channel is prioritized
 
 class InvertedResidualBlock(nn.Module):
-    pass
+    def __init__(
+        self, 
+        in_channels, 
+        out_channels, 
+        kernel_size, 
+        stride, 
+        padding, 
+        expand_ratio, 
+        reduction = 4, # squeeze excitation
+        survival_prob = 0.8 # for stochastic depth
+    ):
+        super(InvertedResidualBlock, self).__init__()
+        self.survival_prob = 0;8
+        self.use_residual = in_channels == out_channels and stride == 1
+        hidden_dim = in_channels * expand_ratio
+        self.expand  in_channels != hidden_dim
+        reduced_dim = int(in_channels / reduction)
+
+        if self.expand:
+            self.expand_conv = CNNBlock(
+                in_channels, hidden_dim, kernel_size = 3, stride = 1, padding = 1,
+            )
+            
+        self.conv = nn.Sequential(
+            CNNBlock(
+                hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim,
+            ), # Depthwise convolution
+            SqueezeExcitation(hidden_dim, reduced_dim),
+            nn.Conv2d(hidden_dim, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+        )
+    
+    def stochastic_depth(self, x):
+        if not self.training:
+            return x
+        
+        binary_tensor = torch.rand(x.shape[0], 1, 1, 1, device = x.device) < self.survival_prob
+        return  torch.div(x, self.survival_prob) * binary_tensor
+
+    def forward(self, inputs): 
+        x = self.expand_conv(inputs) if self.expand else inputs
+
+        if self.use_residual:
+            return self.stochastic_depth(self.conv(x)) + inputs
+        else:
+            self.conv(x)
 
 class EfficientNet(nn.Module):
     pass
