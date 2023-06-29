03b52b0d8350d11211287249ae19c2120f922ce9
Noe Vernier <nvernier-22@gpu5.enst.fr>
2023-06-29 11:13:07 +0200

    Update evaluate_nano

--- evaluate_nano.py
+++ evaluate_nano.py
@@ -1,172 +1,104 @@
 import torch
 import argparse
-from vanilla_lpcvc import LPCVCDataset
-
 from sample_solution.evaluation.accuracy import AccuracyTracker
 from tqdm import tqdm
 from torchvision import transforms as T
 import numpy as np
 from PIL import Image
-from fast_scnn import FastSCNN
-import segmentation_models_pytorch as smp
-from torch2trt import torch2trt
-
-
-print("Finish import packages")
+#from torch2trt import torch2trt
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import random
+print("[Finish import packages]")
 
 IMG_SIZE = 256
 mean = [0.4607, 0.4558, 0.4192]
 std = [0.1855, 0.1707, 0.1769]
-
-ENCODER = 'mobilenet_v2'
-ENCODER_WEIGHTS = 'imagenet'
-N_CLASSES = 14
-ACTIVATION = 'sigmoid'
+colors = ['green', 'red', 'blue', 'yellow', 'orange', 'purple', 'cyan', 'magenta', 'pink', 'lime', 'brown', 'gray', 'olive', 'teal', 'navy']
+cmap = ListedColormap(colors[:14])
 
 accuracyTrackerVal = AccuracyTracker(n_classes=14)
 
-def eval(model, args, val_loader, memory_allocated_prev):
+def load_image(idx, datapath):
+    transform = T.Compose([T.ToTensor(),T.Resize(size=IMG_SIZE, interpolation=T.functional.InterpolationMode.NEAREST)])
+    print("[Loading the image...] at " + datapath)
+    img = Image.open(datapath + '/val/IMG/val_' + str(46).zfill(4) + '.png').convert('RGB')
+    print("[Finish load image]")
+    x = np.array(img)
+    x = transform(x)
+    t = T.Compose([T.Normalize(mean, std)])
+    x = t(x)
+    return torch.tensor(np.expand_dims(x, axis=0)).cuda(), img
 
-    running_time = 0
-    running_memory = 0
-    iteration=0
+def save_image(output, img, save_images_path):
+    output = output.cpu().data.max(1)[1].numpy()
+    output.astype(np.uint8)
+    output = np.squeeze(output, axis=0)
 
-    saved_images = np.zeros((3, IMG_SIZE, IMG_SIZE, 3))
-    print("start timer")
-    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-    print("end timer")
-    with torch.no_grad():
-        print("load loop")
-        loop = tqdm(val_loader)
-        print("finish loop")
-        for batch_idx, (inputs, labels) in enumerate(loop):
-            print("iter")
-            iteration+=1
-            
-            print("swicth input/output to the device")
-            inputs, labels = inputs.to(args.device), labels.to(args.device)
-            
-            print("make inference")
-            starter.record()
-            outputs=model(inputs)
-            ender.record()
-            print("finish inference")
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-            memory_allocated = torch.cuda.memory_allocated(args.device)
-            running_memory += memory_allocated - memory_allocated_prev
+    axes[0].imshow(img.resize((IMG_SIZE, IMG_SIZE)))
+    axes[0].imshow(output, vmin=0, vmax=13, cmap=cmap, alpha=0.4)
+    axes[0].set_xlabel('Prediction')
 
-            torch.cuda.synchronize()
+    axes[1].imshow(img)
 
-            running_time += starter.elapsed_time(ender)
-
-            print("detach from gpu")
-            outputs = outputs.cpu().data.max(1)[1].numpy()
-            labels = labels.cpu().data.max(1)[1].numpy()
-
-            outputs.astype(np.uint8)
-            labels.astype(np.uint8)
-
-            print("update accuracy")
-            accuracyTrackerVal.update(labels, outputs)
-
-
-    val_time = running_time/iteration
-    print("Memory used for one inference : {:.2f} MB".format(running_memory / iteration / (1024 * 1024)))
-
-    return(val_time, saved_images)
-
-
-
-def save_images(saved_images, path):
-    input_image, target_image, pred_image = saved_images[0], saved_images[1], saved_images[2]
-
-    input_image = np.array((input_image*0.1707+0.45)*255, dtype=np.uint8)
-    target_image = np.array(target_image*255, dtype=np.uint8)
-    pred_image = np.array(pred_image*255, dtype=np.uint8)
-
-    Image.fromarray(input_image).save(path+"input.png")
-    Image.fromarray(target_image).save(path+"target.png")
-    Image.fromarray(pred_image).save(path+"pred.png")
-
+    plt.savefig('result.png')
 
 def main():
     parser = argparse.ArgumentParser(description='Information Removal at the bottleneck in Deep Neural Networks')
-    parser.add_argument('--modelpath', default="")
-    parser.add_argument('--datapath', default='LPCVCDataset')
+    parser.add_argument('--modelpath', default="PSPNET_MN_NEWSHED_1063_dice_0.5777767708560559.pth")
+    parser.add_argument('--datapath', default='dataset')
     parser.add_argument('--dev', default="cuda:0")
     parser.add_argument('--save_images_path', default="")
     args = parser.parse_args()
     args.device = torch.device(args.dev)
-
-    print("Finish parsing")
-
     torch.cuda.empty_cache()
-
-    print(args.device)
-
-    #model = torch.load('/home/jetson/Desktop/LPCVC2023/tes2')
-    print("Loading model...")
-    #model = FastSCNN(nclass=14).to(args.device)
-    model = smp.PSPNet(
-        encoder_name=ENCODER, 
-        encoder_weights=ENCODER_WEIGHTS, 
-        classes=N_CLASSES, 
-    ).cuda()
-    print("Loading weights...")
-    model.load_state_dict(torch.load('/home/jetson/Desktop/LPCVC2023/final_model.pt'))    #model= torch.load(args.modelpath, map_location=args.device)
+    #------------------------------------------------------------   
+    print("[Loading model...] at " + args.modelpath)
+    model = torch.load(args.modelpath).cuda()
     model.eval()
-    print("Finish load model")
-    model_int8 = torch.ao.quantization.quantize_dynamic(
-                model,  # the original model
-                {torch.nn.Linear},  # a set of layers to dynamically quantize
-                dtype=torch.qint8)  # the target dtype for quantized weights
-    model_int8.cuda()
-    model_int8.eval()
-    print("Finish Quantization")
-
-    transform = T.Compose([T.ToTensor(),T.Resize(size=IMG_SIZE, interpolation=T.functional.InterpolationMode.NEAREST)])
-    print("Loading dataset...")
-    img = Image.open('/home/jetson/Desktop/LPCVC2023/dataset/val/IMG/val_' + str(0).zfill(4) + '.png').convert('RGB')
-    print("finish load image")
-    img = np.array(img)
-    img = transform(img)
-    t = T.Compose([T.Normalize(mean, std)])
-    img = t(img)
-    print("finish aug")
-    x = torch.tensor(np.expand_dims(img, axis=0)).cuda()
-    print("finish cuda")
-
-    #x = torch.ones((1, 3, 256, 256)).cuda()
-
+    print("[Finish load model]")
+    #------------------------------------------------------------
+    print("[Quantization of the model...]")
+    # model_int8 = torch.ao.quantization.quantize_dynamic(model, {torch.nn.Linear}, dtype=torch.qint8)
+    # model_int8.cuda()
+    # model_int8.eval()
+    print("[Finish Quantization]")
+    #------------------------------------------------------------
+    x, img = load_image(random.randint(1, 100), args.datapath)
+    #------------------------------------------------------------
     # convert to TensorRT feeding sample data as input
-    model_trt = torch2trt(model, [x])
-    print("Finish TensorRT")
-    #model.eval()
-
-
+    print("[Convert the model with TensorRT...]")
+    #model_trt = torch2trt(model, [x])
+    model_trt = model
+    print("[Finish TensorRT]")
+    #------------------------------------------------------------
     starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
 
-    print("finish load sample")
-    #image = torch.tensor(np.expand_dims(img, axis=0)).to(args.device)
-    print("transform to tensor")
+    print("[Start measuring performance...] with 30 inferences")
+    running_inference_time = 0
     with torch.no_grad():
+        # GPU-WARM-UP
+        for _ in range(4):
+            _ = model_trt(x)
+        # MEASURE PERFORMANCE
         for i in range(30):
             starter.record()
             output = model_trt(x)
             ender.record()
             torch.cuda.synchronize()
-            print(starter.elapsed_time(ender))
-    print("predict to tensor")
-    print(output.shape)
-    #output_vis = output.cpu().data.max(1)[1].numpy()
+            running_inference_time += starter.elapsed_time(ender)
+    print("[Finish measuring performance]")
+    print("___________________________________________________")
+    print("______________________RESULTS______________________")
+    print("___________________________________________________")
 
-    # print("finish loas dataset", len(val_dataset))
-    # print("evaluate the model")
-    # val_time, saved_images = eval(model, args, val_loader, memory_allocated_prev)
-    # print(torch.cuda.memory_summary())
-    # print("Total memory used : {:.2f} MB".format(torch.cuda.memory_allocated(args.device) / (1024 * 1024)))
-    # print("inf_time: {:.3f} ms, mean_dice: {:.3f}, score: {:.3f}".format(val_time, accuracyTrackerVal.get_mean_dice(), 1000*accuracyTrackerVal.get_mean_dice()/val_time))
-    # save_images(saved_images, args.save_images_path)
+    print("Mean Inference Time = " + str(round(running_inference_time/30, 3)) + " ms")
+
+    save_image(output, img, args.save_images_path)
+    print("Save results at " + args.save_images_path)
+
 
 if __name__ == '__main__':
     main()
